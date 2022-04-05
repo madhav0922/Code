c6ff36b1915de9172006efde002605bcd6329c45
madhav0922 <madhav.sharma0922@gmail.com>
2022-04-06 01:36:44 +0530

    6 apr - LL

--- Java Practice/Linked_Lists/RotateListByK.java
+++ Java Practice/Linked_Lists/RotateListByK.java
@@ -28,9 +28,38 @@ public class RotateListByK {
         System.out.println();
     }
 
+    public static Node rotateListByK(Node head, int k) {
+        Node h1 = head, second_half, newHead;
+        int i = 0;
+        while (h1 != null && i < k + 1) {
+            h1 = h1.next;
+            i++;
+        }
+        if (h1 == null) {
+            return head;
+        }
+        // printList(h1);
+        second_half = h1.next;
+        // printList(second_half);
+        newHead = second_half;
+        h1.next = null;
+
+        while (second_half.next != null) {
+            second_half = second_half.next;
+        }
+        second_half.next = head;
+        // printList(newHead);
+        return newHead;
+    }
+
     public static void main(String[] args) {
-        Node head = new Node(1, new Node(1, new Node(2, new Node(3, new Node(4,
-                new Node(5, null))))));
+        Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5,
+                new Node(6, null))))));
+
+        Node head2 = new Node(1, new Node(2, null));
+
+        Node result = rotateListByK(head, 2);
+        printList(result);
     }
 
 }
